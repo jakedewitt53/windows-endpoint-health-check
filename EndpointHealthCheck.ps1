59483087f4c98543e5be60a53893f39e51065a89
jakedewitt53 <jakedewitt53@gmail.com>
2026-09-08 18:28:02 -0400

    Add computer and user detection

--- EndpointHealthCheck.ps1
+++ EndpointHealthCheck.ps1
@@ -1,5 +1,5 @@
 #Windows Endpoint Health Check
-#Version 0.1
+#Version 0.2
 Personal PowerShell / Endpoint Administration Lab
 
 Write-Host "======================================="
@@ -7,4 +7,11 @@ Write-Host"      Windows EndPoint Health Check
 Write-Host "======================================="
 Write-Host ""
 
+$ComputerName = $env:COMPUTERNAME
+$ComputerName = $env:USERNAME
+
+Write-Host "Computer Name: $ComputerName"
+Write-Host "Computer User: $CurrentUser"
+
+Write-Host ""
 Write-Host "Health check started..."
