4c1a08a6e3b41a57fabf252475bdaf1e32be9016
jakedewitt53 <jakedewitt53@gmail.com>
2026-09-08 18:22:16 -0400

    Add initial endpoint health check script

---
+++ EndpointHealthCheck.ps1
@@ -0,0 +1,10 @@
+#Windows Endpoint Health Check
+#Version 0.1
+Personal PowerShell / Endpoint Administration Lab
+
+Write-Host "======================================="
+Write-Host"      Windows EndPoint Health Check
+Write-Host "======================================="
+Write-Host ""
+
+Write-Host "Health check started..."
