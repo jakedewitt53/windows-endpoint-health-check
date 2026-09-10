e69d1c18bc5b40fe460db395385812b57c2d0245
Jacob DeWitt <jakedewitt53@gmail.com>
2026-09-10 14:57:39 -0400

    Add hardware and disk inventory

--- EndpointHealthCheck.ps1
+++ EndpointHealthCheck.ps1
@@ -10,12 +10,21 @@ Write-Host ""
 $ComputerName = $env:COMPUTERNAME
 $CurrentUser = $env:USERNAME
 $OS = Get-CimInstance Win32_OperatingSystem
+$ComputerSystem = Get-CimInstance Win32_ComputerSystem
+$Processor = Get-CimInstance Win32_Processor
+$Disk = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'"
 
 Write-Host "Computer Name: $ComputerName"
 Write-Host "Computer User: $CurrentUser"
 Write-Host "Operating System: $($OS.Caption)"
 Write-Host "OS Version: $($OS.Version)"
 Write-Host "Build Number: $($OS.BuildNumber)"
+Write-Host "Manufacturer: $($ComputerSystem.Manufacturer)"
+Write-Host "Model: $($ComputerSystem.Model)"
+Write-Host "Processor: $($Processor.Name)"
+Write-Host "Installed RAM: $([math]::Round($ComputerSystem.TotalPhysicalMemory / 1GB, 2)) GB"
+Write-Host "C: Drive Size: $([math]::Round($Disk.Size / 1GB, 2)) GB"
+Write-Host "C: Drive Free: $([math]::Round($Disk.FreeSpace / 1GB, 2)) GB"
 
 Write-Host ""
-Write-Host "Health check started..."
+Write-Host "Health check complete..."
